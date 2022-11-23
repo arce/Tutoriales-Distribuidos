cc2e16de29ee0c23c8e21f21112c184fe71588a3
Armando Arce <armando.arce@gmail.com>
2022-11-23 07:47:29 -0600

    Create bookInsert.js

---
+++ Tutorial14/netlify/functions/bookInsert.js
@@ -0,0 +1,24 @@
+"use strict"
+const redis = require('./redisDB');
+const headers = require('./headersCORS');
+function toJson(item, index, arr) {
+  arr[index] = JSON.parse(item);
+}
+exports.handler = async (event, context) => {
+  if (event.httpMethod == "OPTIONS") {
+    return { statusCode: 200, headers, body: "OK" };
+  }
+  try {
+    
+    redis.on("connect", function() {
+      console.log("You are now connected");
+    });
+   
+   const data = JSON.parse(event.body);
+   await redis.put(data.id,event.body);
+   return { statusCode: 200, headers, body: 'OK'};
+  } catch (error) {
+    console.log(error);
+    return { statusCode: 400, headers, body: JSON.stringify(error) };
+  }
+};
