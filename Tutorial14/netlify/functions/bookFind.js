cb052ab03779fc6aa91a088fcebc693350272376
Armando Arce <armando.arce@gmail.com>
2022-11-23 07:46:43 -0600

    Create bookFind.js

---
+++ Tutorial14/netlify/functions/bookFind.js
@@ -0,0 +1,27 @@
+"use strict"
+const redis = require('./redisDB');
+const headers = require('./headersCORS');
+function toJson(item, index, arr) {
+  arr[index] = JSON.parse(item);
+}
+exports.handler = async (event, context) => {
+  if (event.httpMethod == "OPTIONS") {
+    return { statusCode: 200, headers, body: "OK" };
+  }
+  try {
+    
+    const id = event.path.split("/").reverse()[0];
+    
+    redis.on("connect", function() {
+      console.log("You are now connected");
+    });
+   const book = await redis.get(id);
+   let books = [];
+   books.push(book);
+   books.forEach(toJson);
+    return { statusCode: 200, headers, body: JSON.stringify(books)};
+  } catch (error) {
+    console.log(error);
+    return { statusCode: 400, headers, body: JSON.stringify(error) };
+  }
+};
